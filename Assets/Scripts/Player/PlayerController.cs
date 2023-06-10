3d72b20d28fdc29d83915bfb841074e963bd62d9
Oğulcan Durkan <88947242+Ogubenn@users.noreply.github.com>
2023-06-10 14:53:37 +0300

    LeftRight

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,12 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Player")]
     [SerializeField] float speed = 3f; //Player baþlangýç hýzý
-    Rigidbody rb;
     Vector3 direction = Vector3.forward; //playerýn hareketinin yönü
     public float difficult = 0.03f; //zaman geçtikçe playerýn hýzý artmasý katsayýsý
 
     [Header("GameManager")]
     public GroundSpawner groundSpawner; // Ground Make e ulaþmak için ground spawner scripti
+    public float leftBoundary = -3f; //Playerýn sola max ne kadar gidebileceði default -1.5f
+    public float rightBoundary = 3f; //Playerýn saða max ne kadar gidebileceði default 1.6f
+    public float leftRightSpeed = 4f; //Saða sola gitme hýzý default 5
+    Rigidbody rb;
+
+
 
     private void Awake()
     {
@@ -23,8 +28,20 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = direction * speed * Time.deltaTime; //hareket deðeri
         speed += Time.deltaTime * difficult; //zorluk katsayýsýný speede ekliyoruz.
         transform.position += movement; //hareket deðerini sürekli pozisyona ekler
+
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.gameObject.transform.position.x > leftBoundary)
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.gameObject.transform.position.x < rightBoundary)
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+        }
     }
 
+    #region groundExit
     private void OnCollisionExit(Collision collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
@@ -42,4 +59,6 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(0.4f);
         Destroy(groundparam); // 0.4 sn bekleyip destroyluyoruz.
     }
+
+    #endregion
 }//class
