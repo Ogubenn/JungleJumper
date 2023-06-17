e36901bc0c78d2a3da82800bee04934de012a5a4
Oğulcan Durkan <88947242+Ogubenn@users.noreply.github.com>
2023-06-18 00:07:29 +0300

    Crash

--- Assets/Scripts/GameManager/CountDownTimer.cs
+++ Assets/Scripts/GameManager/CountDownTimer.cs
@@ -7,33 +7,27 @@ public class CountDownTimer : MonoBehaviour
 {
     public int countDownTimer;
     public Text countDownDisplay;
-    public static bool isGo = false;
 
     private void Start()
     {
-        StartCoroutine(CountDown());
+        StartCoroutine(StartCountdown());
     }
 
-    IEnumerator CountDown()
+    IEnumerator StartCountdown()
     {
-        while(countDownTimer > 0)
+        countDownDisplay.text = countDownTimer.ToString();
+        yield return new WaitForSecondsRealtime(1f);
+
+        while (countDownTimer > 0)
         {
-            countDownDisplay.text = countDownTimer.ToString();
-            yield return new WaitForSeconds(1f);
             countDownTimer--;
+            countDownDisplay.text = countDownTimer.ToString();
+            yield return new WaitForSecondsRealtime(1f);
         }
-        countDownDisplay.text = "GO";
-        yield return new WaitForSeconds(1f);
+
+        countDownDisplay.text = "GO!!!";
+        yield return new WaitForSecondsRealtime(1f);
+
         countDownDisplay.gameObject.SetActive(false);
-        TextActive();
     }
-
-    public void TextActive()
-    {
-        Time.timeScale = 1;
-        isGo = true;
-    }
-
-
-
-}//class
+}
