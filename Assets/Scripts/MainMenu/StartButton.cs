59a589a35a76c5343be7de14fe6f7349a69df24f
Oğulcan Durkan <88947242+Ogubenn@users.noreply.github.com>
2023-06-20 11:55:36 +0300

    Main Menu Ok

---
+++ Assets/Scripts/MainMenu/StartButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Go");
+        SceneManager.LoadScene(1);
+    }
+
+}//class
